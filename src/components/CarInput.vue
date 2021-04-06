4952ff68017247be463c8a2f7afd0f827741592d
Davide Baldelli <davbaldelli@gmail.com>
2021-04-06 19:31:33 +0200

    implementato nazione/brand esistente/nuovo

--- src/components/CarInput.vue
+++ src/components/CarInput.vue
@@ -28,8 +28,38 @@
             required
         ></b-form-input>
       </b-form-group>
-      <b-form-select @change="onBrandChange()" v-model="form.Brand.Name" :options="brandsOpts" class="mb-3"/>
-      <b-form-select v-model="form.Brand.Nation.Name" :options="nationOptions" class="mb-3"/>
+      <b-form-select v-if="existingBrand" @change="onBrandChange()" v-model="form.Brand.Name" :options="brandsOpts" class="mb-3"/>
+      <b-form-group
+          id="input-group-3"
+          label="Brand Name:"
+          label-for="input-3"
+          descripiton="Enter new Brand Name"
+          v-if="!existingBrand"
+      >
+        <b-form-input
+            id="input-3"
+            v-model="form.Brand.Name"
+            placeholder="Enter new Brand Name"
+            required
+        ></b-form-input>
+      </b-form-group>
+      <b-form-checkbox v-model="existingBrand" name="check-button-brand" switch>Existing Brand</b-form-checkbox>
+      <b-form-group
+        id="input-group-4"
+        label="Nation Name:"
+        label-for="input-4"
+        descripiton="Enter new Nation"
+        v-if="!existingNation && !existingBrand"
+      >
+        <b-form-input
+            id="input-3"
+            v-model="form.Brand.Nation.Name"
+            placeholder="Enter new Nation"
+            required
+        ></b-form-input>
+      </b-form-group>
+      <b-form-select v-if="existingNation && !existingBrand"  v-model="form.Brand.Nation.Name" :options="nationOptions" class="mb-3"/>
+      <b-form-checkbox v-if="!existingBrand" v-model="existingNation" name="check-button-nation" switch>Existing Nation</b-form-checkbox>
       <b-form-group
           id="form-cat-group"
           label="Enter categories"
@@ -44,7 +74,6 @@
       <b-button @click="addNewCategory">Add Category</b-button>
       <b-button type="submit" variant="primary">Submit</b-button>
     </b-form>
-    <pre>{{ form }}</pre>
   </div>
 
 </template>
@@ -57,18 +86,21 @@ export default {
       selected: null,
       form : {
         DownloadLink : 'https://mega.nz',
-        ModelName : 'F2005',
+        ModelName : '',
         Brand : {
-          Name : "Ferrari",
+          Name : "",
           Nation : {
-            Name : null
+            Name : ""
           }
         },
-        Categories : ["F1"]
+        Categories : [""]
       },
       brandsOpts: [],
-      brandNations : [],
-      nationOptions : []
+      nationOptions: [],
+      brands : [],
+      nations: [],
+      existingBrand : true,
+      existingNation : true,
     }
   },
   methods: {
@@ -81,23 +113,28 @@ export default {
     addNewCategory(){
       this.form.Categories.push('')
     },
-    addNewBrand(brandName){
+    addBrandOpts(brandName){
       this.brandsOpts.push(JSON.parse('{"value" : "'+brandName+'", "text" : "'+brandName+'"}'));
+    },
+    addNationOpt(nationName){
+      this.nationOptions.push(JSON.parse('{"value" : "'+nationName+'", "text" : "'+nationName+'"}'));
     },
     onBrandChange(){
       this.nationOptions = []
       var nation = ""
-      this.brandNations.forEach( brand => {
+      this.brands.forEach(brand => {
         if (brand.Name === this.form.Brand.Name){
           nation = brand.Nation;
         }
       });
-      this.nationOptions.push(JSON.parse('{"value" : "'+nation+'", "text" : "'+nation+'"}'))
+      this.form.Brand.Nation.Name = nation
     }
   },
   mounted() {
     this.axios.get("http://localhost:6316/brand/all")
-    .then(res => {this.brandNations = res.data ; res.data.forEach( val =>this.addNewBrand(val.Name))});
+    .then(res => {this.brands = res.data ; res.data.forEach(val =>this.addBrandOpts(val.Name))});
+    this.axios.get("http://localhost:6316/nation/brand/all")
+    .then(res => {this.nations = res.data; res.data.forEach(res => this.addNationOpt(res.Name))})
   }
 
 }
