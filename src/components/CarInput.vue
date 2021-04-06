8e90402095d8bd7f323b1dfc0c41ff6dde882cb4
Davide Baldelli <davbaldelli@gmail.com>
2021-04-06 17:46:32 +0200

    selezione brand e nation vincolata

--- src/components/CarInput.vue
+++ src/components/CarInput.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <b-form @submit="onSubmit()">
+    <b-form @submit.prevent="onSubmit()">
       <b-form-group
         id="input-group-1"
         label="Car Model:"
@@ -28,32 +28,8 @@
             required
         ></b-form-input>
       </b-form-group>
-      <b-form-group
-          id="input-group-3"
-          label="Car Brand:"
-          label-for="input-3"
-          descripiton="Enter car brand"
-      >
-        <b-form-input
-            id="input-3"
-            v-model="form.Brand.Name"
-            placeholder="Enter car brand"
-            required
-        ></b-form-input>
-      </b-form-group>
-      <b-form-group
-          id="input-group-4"
-          label="Car Brand Nation:"
-          label-for="input-4"
-          descripiton="Enter car brand nation"
-      >
-        <b-form-input
-            id="input-4"
-            v-model="form.Brand.Nation.Name"
-            placeholder="Enter car brand nation"
-            required
-        ></b-form-input>
-      </b-form-group>
+      <b-form-select @change="onBrandChange()" v-model="form.Brand.Name" :options="brandsOpts" class="mb-3"/>
+      <b-form-select v-model="form.Brand.Nation.Name" :options="nationOptions" class="mb-3"/>
       <b-form-group
           id="form-cat-group"
           label="Enter categories"
@@ -66,7 +42,6 @@
         </div>
       </b-form-group>
       <b-button @click="addNewCategory">Add Category</b-button>
-      <b-button @click="sendData">Send data</b-button>
       <b-button type="submit" variant="primary">Submit</b-button>
     </b-form>
     <pre>{{ form }}</pre>
@@ -79,17 +54,21 @@ export default {
   name: "CarInput",
   data(){
     return {
+      selected: null,
       form : {
         DownloadLink : 'https://mega.nz',
         ModelName : 'F2005',
         Brand : {
-          Name : 'Ferrari',
+          Name : "Ferrari",
           Nation : {
-            Name : 'Italy'
+            Name : null
           }
         },
         Categories : ["F1"]
-      }
+      },
+      brandsOpts: [],
+      brandNations : [],
+      nationOptions : []
     }
   },
   methods: {
@@ -99,16 +78,28 @@ export default {
       .then(res => console.log(res))
       .catch(err => console.log(err))
     },
-    sendData(){
-      this.axios
-          .post("http://localhost:6316/car/new",this.form)
-          .then(res => console.log(res))
-          .catch(err => console.log(err))
-    },
     addNewCategory(){
       this.form.Categories.push('')
+    },
+    addNewBrand(brandName){
+      this.brandsOpts.push(JSON.parse('{"value" : "'+brandName+'", "text" : "'+brandName+'"}'));
+    },
+    onBrandChange(){
+      this.nationOptions = []
+      var nation = ""
+      this.brandNations.forEach( brand => {
+        if (brand.Name === this.form.Brand.Name){
+          nation = brand.Nation;
+        }
+      });
+      this.nationOptions.push(JSON.parse('{"value" : "'+nation+'", "text" : "'+nation+'"}'))
     }
+  },
+  mounted() {
+    this.axios.get("http://localhost:6316/brand/all")
+    .then(res => {this.brandNations = res.data ; res.data.forEach( val =>this.addNewBrand(val.Name))});
   }
+
 }
 </script>
 
