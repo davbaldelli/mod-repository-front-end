8f6338c3e9742b83e5406e4a11a6b900ea6e7e71
Davide Baldelli <davbaldelli@gmail.com>
2021-11-12 10:42:58 +0100

    showing last 10 added cars

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -86,28 +86,12 @@ export default {
     }
   },
   mounted() {
-    this.$store.dispatch('cars/getAll').then(() => this.cars = carsFilters.lastAdded(5)(this.$store.getters["cars/cars"]))
+    this.$store.dispatch('cars/getAll').then(() => this.cars = carsFilters.lastAdded(10)(this.$store.getters["cars/cars"]))
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
